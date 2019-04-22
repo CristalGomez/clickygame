b6bda92cf1d8db6db25b621c7a94f9022c84bcfc
Cristal Gomez <cristalsg1@gmail.com>
2019-04-21 22:32:14 -0500

    uploading everything

---
+++ build/precache-manifest.eeab4959573c3c3a721614397bc37662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "c53c853d3f6866e03088",
+    "url": "/clickygame/static/js/main.4c7eb6a9.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clickygame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "c53c853d3f6866e03088",
+    "url": "/clickygame/static/css/main.41116eb2.chunk.css"
+  },
+  {
+    "revision": "a5095ebfe1e9f162895eaed3ea4fcfc1",
+    "url": "/clickygame/index.html"
+  }
+];
